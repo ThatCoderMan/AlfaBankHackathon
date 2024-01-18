9a3a587fabfb986f80bba8721aa11e6d0a1616f1
Василий Стакроцкий <inferno681@gmail.com>
2024-01-18 04:16:47 +0100

    all modells ready(1st try)

--- app/models/base.py
+++ app/models/base.py
@@ -1,5 +1,25 @@
-from app.core.db import Base
+from datetime import datetime
+
+from sqlalchemy import Column, Date, DateTime, Integer
+from sqlalchemy.orm import declarative_base, declared_attr
 
 
-class BaseAbstractModel(Base):
+class BaseModel:
+    """Основа для таблиц БД"""
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, default=datetime.now)
+
+
+Base = declarative_base(cls=BaseModel)
+
+
+class AbstractDatesModel(Base):
+    """Абстрактная модель с датами начала и окончания"""
     __abstract__ = True
+    starting_date = Column(Date(), nullable=False)
+    deadline = Column(Date(), nullable=False)
