b631b2788ebd575ec0dcf0a21892723d00b280d3
artemii <roke.berezin@gmail.com>
2024-01-18 17:55:00 +0300

    Refactoring: добавлено версионирование в архитектуру

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from app.api.routers import main_router
+from app.api.routers import api_router
 from app.auth.routers import auth_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_title, description=settings.description)
-app.include_router(main_router)
+app.include_router(api_router)
 app.include_router(auth_router)
