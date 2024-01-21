ca137bece8e59217948313555c5d89fa6fc9da1c
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2024-01-22 00:19:53 +0300

    started working with routes

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -1,3 +1,43 @@
 from fastapi import APIRouter
 
 api_v1_router = APIRouter()
+
+
+@api_v1_router.get('/main/')
+async def get_homepage():
+    return {None}
+
+
+@api_v1_router.get('/my_employees/')
+def get_employees():
+    return {None}
+
+
+@api_v1_router.get('/pdp/{pdp_id}')
+async def get_pdp():
+    return {None}
+
+
+@api_v1_router.patch('/pdp/{pdp_id}')
+async def change_pdp():
+    return {None}
+
+
+@api_v1_router.get('pdp/{pdp_id}/{task_id}/')
+async def get_task():
+    return {None}
+
+
+@api_v1_router.post('pdp/{pdp_id}/{task_id}/')
+async def create_task():
+    return {None}
+
+
+@api_v1_router.patch('pdp/{pdp_id}/{task_id}/')
+async def change_task():
+    return {None}
+
+
+@api_v1_router.post('/creating_request/')
+async def create_request():
+    return {None}
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from app.api.routers import api_router
@@ -7,3 +8,7 @@ from app.core.config import settings
 app = FastAPI(title=settings.app_title, description=settings.description)
 app.include_router(api_router)
 app.include_router(auth_router)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
