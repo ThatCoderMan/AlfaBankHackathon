2b4996f19ef78389e909182edddb0d4ec3c8ef48
VladislavCR <vladislav.kuznetsov.cropp@gmail.com>
2024-01-22 00:46:31 +0300

    fixed routes

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -23,21 +23,26 @@ async def change_pdp():
     return {None}
 
 
-@api_v1_router.get('pdp/{pdp_id}/{task_id}/')
+@api_v1_router.get('/pdp/{pdp_id}/{task_id}/')
 async def get_task():
     return {None}
 
 
-@api_v1_router.post('pdp/{pdp_id}/{task_id}/')
+@api_v1_router.post('/pdp/{pdp_id}/{task_id}/')
 async def create_task():
     return {None}
 
 
-@api_v1_router.patch('pdp/{pdp_id}/{task_id}/')
+@api_v1_router.patch('/pdp/{pdp_id}/{task_id}/')
 async def change_task():
     return {None}
 
 
-@api_v1_router.post('/creating_request/')
-async def create_request():
+@api_v1_router.get('/statuses/')
+async def get_status():
+    return {None}
+
+
+@api_v1_router.get('types_of_tasks')
+async def get_type_of_task():
     return {None}
