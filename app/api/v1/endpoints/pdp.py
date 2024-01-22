0463aac437421b26ca29c99b4d0b33aee3794480
artemii <roke.berezin@gmail.com>
2024-01-22 20:18:49 +0300

    fix: pdp router

--- app/api/v1/endpoints/pdp.py
+++ app/api/v1/endpoints/pdp.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
-from app.core.user import current_chief, current_user
+from app.core.user import current_user
 from app.models import User
 from app.schemas import PDPRead, PDPUpdate
 
@@ -29,6 +29,6 @@ async def change_pdp(
     pdp_id: int,
     pdp_obj: PDPUpdate,
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_chief),
+    user: User = Depends(current_user),
 ):
     return {None}
