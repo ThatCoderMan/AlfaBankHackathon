16d1f5547823baf4b96c08f5480d83387eaa455b
Василий Стакроцкий <inferno681@gmail.com>
2024-01-25 13:25:23 +0100

    nothing

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from .pdp import PDP
+from .pdp import LENGTH_LIMITS_STRING_FIELDS, PDP
 from .status import Status
 from .task import Task
 from .type import Type
--- app/models/status.py
+++ app/models/status.py
@@ -6,14 +6,14 @@ from app.core.db import Base
 
 from .user import UserRole
 
-LENGTH_LIMITS_TYPE_NAME_FIELD = 50
+LENGTH_LIMITS_STATUS_NAME_FIELD = 50
 
 
 class Status(Base):
     """Модель навыков"""
 
     name = Column(
-        String(LENGTH_LIMITS_TYPE_NAME_FIELD), nullable=False, unique=True
+        String(LENGTH_LIMITS_STATUS_NAME_FIELD), nullable=False, unique=True
     )
     role = Column(
         PgEnum(UserRole, name='userrole', create_type=False),
