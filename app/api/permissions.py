3fd63db007f29bafbe3761a73f8c967543a81240
Василий Стакроцкий <inferno681@yandex.ru>
2024-01-28 00:55:34 +0100

    permissions before rebase

--- app/api/permissions.py
+++ app/api/permissions.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.user import current_user
-from app.crud import pdp_crud, user_crud
+from app.crud import pdp_crud, task_crud, user_crud
 from app.models import User
 from app.models.user import UserRole
 
@@ -18,7 +18,7 @@ def only_chief_accesses(user: User = Depends(current_user)):
     return user
 
 
-async def is_pdp_owner(
+async def is_pdp_owner_or_chief(
     pdp_id: int,
     user: User = Depends(current_user),
     session: AsyncSession = Depends(get_async_session)
@@ -31,9 +31,41 @@ async def is_pdp_owner(
     pdp = await pdp_crud.get(session=session, pdp_id=pdp_id)
     if pdp is None:
         raise HTTPException(
-            status_code=404, detail='ИПР с id {pdp_id} не существует')
-    pdp_owner = user_crud.get(session=session, obj_id=pdp.user_id)
-    if pdp_owner.chief_id != user.id and pdp.user_id != user.id:
+            status_code=404,
+            detail=f'ИПР с id {pdp_id} не существует'
+        )
+    is_chief = await user_crud.is_chief(
+        session=session, chief_id=user.id, user_id=pdp.user_id)
+    if not is_chief and pdp.user_id != user.id:
         raise HTTPException(
-            status_code=403, detail='Доступ к чужим ИПР запрещен')
+            status_code=403,
+            detail='Недостаточно прав для доступа к этому ИПР'
+        )
     return pdp
+
+
+async def is_task_owner_or_chief(
+    task_id: int,
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session)
+):
+    if user.id is None:
+        raise HTTPException(
+            status_code=401,
+            detail='Недоступно неавторизованным пользователям'
+        )
+    task = await task_crud.get(session=session, task_id=task_id)
+    pdp = await pdp_crud.get(session=session, pdp_id=task.pdp_id)
+    if task is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f'задача с id {task_id} не существует'
+        )
+    is_chief = await user_crud.is_chief(
+        session=session, chief_id=user.id, user_id=pdp.user_id)
+    if not is_chief and pdp.user_id != user.id:
+        raise HTTPException(
+            status_code=403,
+            detail='Недостаточно прав для доступа к этой задаче'
+        )
+    return task
--- app/api/v1/endpoints/task.py
+++ app/api/v1/endpoints/task.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.permissions import only_chief_accesses
+from app.api.permissions import only_chief_accesses, is_task_owner_or_chief
 from app.core.db import get_async_session
 from app.core.user import current_user
 from app.crud.task import task_crud
@@ -12,8 +12,11 @@ router = APIRouter()
 
 # Todo: permission У текущего пользователя есть доступ к задаче
 #  (шеф или сотрудник) таск_id == user_id and role == chief где его сотрудники
-@router.get('/{task_id}', response_model=TaskRead,
-            dependencies=[Depends(current_user)])
+@router.get(
+    '/{task_id}',
+    response_model=TaskRead,
+    dependencies=[Depends(current_user), Depends(is_task_owner_or_chief)]
+)
 async def get_task(
         task_id: int,
         session: AsyncSession = Depends(get_async_session),
@@ -23,8 +26,11 @@ async def get_task(
 
 # Todo: проверка полей при создании руководителем,
 #  или сотрудником, меняются поля создания задачи
-@router.post('/', response_model=TaskRead,
-             dependencies=[Depends(current_user)])
+@router.post(
+    '/',
+    response_model=TaskRead,
+    dependencies=[Depends(current_user), Depends(is_task_owner_or_chief)]
+)
 async def create_task(
         task_in: TaskCreate,
         session: AsyncSession = Depends(get_async_session),
