511282ce347d3b5df41af09604757f764e993ad5
Серей Овинцев <ozonti@mail.ru>
2024-01-31 10:53:19 +0300

    fix: email secrets

--- app/core/config.py
+++ app/core/config.py
@@ -7,15 +7,15 @@ class Settings(BaseSettings):
     secret: str = "SECRET"
     database_url: str = "sqlite+aiosqlite:///./fastapi.db"
 
-    mail_username: str = 'AlfaBankHackathon@ya.ru'
-    mail_password: str = 'yfidmxkvwehwyhju'  # noqa
-    mail_from: str = 'AlfaBankHackathon@ya.ru'
-    mail_port: int = 465
-    mail_server: str = 'smtp.yandex.ru'
-    mail_starttls: bool = False
-    mail_ssl_tls: bool = True
-    use_credentials: bool = True
-    validate_certs: bool = False
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_port: int
+    mail_server: str
+    mail_starttls: bool
+    mail_ssl_tls: bool
+    use_credentials: bool
+    validate_certs: bool
 
     class Config:
         env_file = ".env"
