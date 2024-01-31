c8db658bc25c066d9e4db0c07f5b5901b86afdd4
Серей Овинцев <ozonti@mail.ru>
2024-01-31 10:44:10 +0300

    add: confing for mail

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,16 @@ class Settings(BaseSettings):
     secret: str = "SECRET"
     database_url: str = "sqlite+aiosqlite:///./fastapi.db"
 
+    mail_username: str = 'AlfaBankHackathon@ya.ru'
+    mail_password: str = 'yfidmxkvwehwyhju'  # noqa
+    mail_from: str = 'AlfaBankHackathon@ya.ru'
+    mail_port: int = 465
+    mail_server: str = 'smtp.yandex.ru'
+    mail_starttls: bool = False
+    mail_ssl_tls: bool = True
+    use_credentials: bool = True
+    validate_certs: bool = False
+
     class Config:
         env_file = ".env"
 
