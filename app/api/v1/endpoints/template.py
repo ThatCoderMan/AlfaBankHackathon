d5f31c7253d18b9af09b25c88e31c0e2f1ef812e
artemii <roke.berezin@gmail.com>
2024-02-01 08:54:20 +0300

    Add: template permissions

--- app/api/v1/endpoints/template.py
+++ app/api/v1/endpoints/template.py
@@ -3,10 +3,12 @@ from pydantic import PositiveInt
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.permissions import only_chief_accesses
+from app.core import exceptions
 from app.core.db import get_async_session
 from app.core.user import current_user
 from app.crud.template import template_crud
-from app.models import User
+from app.models import User, Template
 from app.schemas import (
     TaskFromTemplateCreate,
     TemplateCreate,
@@ -21,15 +23,15 @@ router = APIRouter()
 
 @router.get('/', response_model=list[TemplateRead])
 async def get_templates(
-    q: str | None = None,
-    direction: PositiveInt | None = None,
-    skills: list[PositiveInt] = Query(None),
-    grade: list[PositiveInt] = Query(None),
-    type: PositiveInt | None = None,
-    duration_from: PositiveInt | None = None,
-    duration_to: PositiveInt | None = None,
-    creator: PositiveInt | None = None,
-    session: AsyncSession = Depends(get_async_session),
+        q: str | None = None,
+        direction: PositiveInt | None = None,
+        skills: list[PositiveInt] = Query(None),
+        grade: list[PositiveInt] = Query(None),
+        type: PositiveInt | None = None,
+        duration_from: PositiveInt | None = None,
+        duration_to: PositiveInt | None = None,
+        creator: PositiveInt | None = None,
+        session: AsyncSession = Depends(get_async_session),
 ):
     return await template_crud.get_multi(
         session=session,
@@ -46,17 +48,27 @@ async def get_templates(
 
 @router.get('/{template_id}', response_model=TemplateRead)
 async def get_template(
-    template_id: int,
-    session: AsyncSession = Depends(get_async_session),
+        template_id: int,
+        session: AsyncSession = Depends(get_async_session),
 ):
-    return await template_crud.get(template_id=template_id, session=session)
+    template_obj = await template_crud.get(
+        template_id=template_id,
+        session=session
+    )
+    if template_obj is None:
+        raise exceptions.NotExistException(model=Template, pk=template_id)
+    return template_obj
 
 
-@router.post('/', response_model=TemplateRead)
+@router.post(
+    '/',
+    response_model=TemplateRead,
+    dependencies=[Depends(only_chief_accesses)]
+)
 async def create_template(
-    template_in: TemplateCreate,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_user),
+        template_in: TemplateCreate,
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user),
 ):
     template_obj = await template_crud.create(
         session=session, obj_in=template_in, user_id=user.id
@@ -69,38 +81,50 @@ async def create_template(
 @router.patch(
     '/{template_id}',
     response_model=TemplateRead,
+    dependencies=[Depends(only_chief_accesses)]
 )
 async def change_template(
-    template_id: int,
-    template_in: TemplateUpdate,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_user),
+        template_id: int,
+        template_in: TemplateUpdate,
+        session: AsyncSession = Depends(get_async_session),
 ):
     template_db = await template_crud.get(
         template_id=template_id, session=session
     )
+    if template_db is None:
+        raise exceptions.NotExistException(model=Template, pk=template_id)
     return await template_crud.update(
         session=session, obj_in=template_in, db_obj=template_db
     )
 
 
-@router.delete('/{template_id}', status_code=204, deprecated=True)
+@router.delete(
+    '/{template_id}',
+    status_code=204,
+    deprecated=True,
+    dependencies=[Depends(only_chief_accesses)]
+)
 async def delete_template(
-    template_id: int,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_user),
+        template_id: int,
+        session: AsyncSession = Depends(get_async_session),
 ):
     template_obj = await template_crud.get(
         template_id=template_id, session=session
     )
+    if template_obj is None:
+        raise exceptions.NotExistException(model=Template, pk=template_id)
     await template_crud.remove(db_obj=template_obj, session=session)
-    return {'message': 'ok'}
+    return {'details': 'ok'}
 
 
-@router.post('/set_users', status_code=201)
+@router.post(
+    '/set_users',
+    status_code=201,
+    dependencies=[Depends(only_chief_accesses)]
+)
 async def set_template_to_users(
-    data: TaskFromTemplateCreate,
-    session: AsyncSession = Depends(get_async_session),
+        data: TaskFromTemplateCreate,
+        session: AsyncSession = Depends(get_async_session),
 ):
     await create_tasks_from_template(
         session=session,
@@ -109,11 +133,15 @@ async def set_template_to_users(
     )
 
 
-@router.post('/create_from_task', status_code=201)
+@router.post(
+    '/create_from_task',
+    status_code=201,
+    dependencies=[Depends(only_chief_accesses)]
+)
 async def create_from_task(
-    data: TemplateFromTaskCreate,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_user),
+        data: TemplateFromTaskCreate,
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user),
 ):
     await create_template_from_task(
         session=session, task_id=data.task_id, user=user
