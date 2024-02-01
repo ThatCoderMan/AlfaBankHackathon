99b1545563bdf6a7689887b53930b130f7bb54f2
artemii <roke.berezin@gmail.com>
2024-02-01 09:00:03 +0300

    fix: configs

--- app/core/config.py
+++ app/core/config.py
@@ -9,11 +9,11 @@ class Settings(BaseSettings):
         "postgresql+asyncpg://postgres:postgres@db:5432/postgres"
     )
 
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int
-    mail_server: str
+    mail_username: str = "mail_username"
+    mail_password: str = "mail_password"
+    mail_from: str = "example@mail.com"
+    mail_port: int = 465
+    mail_server: str = "mail_server"
     mail_starttls: bool = False
     mail_ssl_tls: bool = True
     use_credentials: bool = True
