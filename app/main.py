44c2123366783a337551321bf36afa3d867c1e69
Серей Овинцев <ozonti@mail.ru>
2024-02-03 22:46:51 +0300

    Update: add cors "origins" to middeleware

--- app/main.py
+++ app/main.py
@@ -23,17 +23,9 @@ app = FastAPI(
     openapi_tags=tags_metadata,
 )
 
-origins = [
-    'http://localhost',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'http://alfabankhack.ddns.net',
-    'http://alfabankhack.ddns.net:8000/',
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
